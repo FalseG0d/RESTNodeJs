0db447c1d485efdda6c1768d3a585cd5f0f89b96
Apoorv Garg <u.garg.10@gmail.com>
2020-03-08 16:51:19 +0530

    Routes Established

--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,71 @@
 const router=require("express").Router();
+const mongoose=require("mongoose");
 
-router.get("/",(req,res)=>{
-    res.send({
-        ok:true
-    })
-});
+
+const Post=mongoose.model("Post");
+
+
+
+//Dummy post request
+router.post("/",async(req,res)=>{
+    try{
+        const post=new Post();
+        post.title=req.body.title;
+        post.content=req.body.content;
+        await post.save();
+        res.send(post);
+    }catch(error){
+        res.send({"error":error});
+    }
+})
+
+//Dummy get request
+router.get("/",async(req,res)=>{
+    try{
+        const posts=await Post.find({});
+        res.send(posts);
+    }catch(error){
+        res.status(500);
+    }
+})
+
+//Dummy get request wih filter
+router.get("/:postId",async (req,res)=>{
+    try{
+        const post=await Post.findOne({_id:req.params.postId})//find for all the entries and findOne for first occurence
+        res.send(post);
+    }catch(error){
+        res.status(500);
+    }
+})
+
+//Dummy put request
+router.put("/:postId",async(req,res)=>{
+    try{
+        const post=await Post.findByIdAndUpdate({
+            _id:req.params.postId
+        },req.body,{
+            new:true, // If new:true in addidtional information then
+            runValidater:true
+        })       
+        res.send(post)//the post object sent here is updated otherwise it is the original one but updates are made regardless  
+    }catch(error){
+        res.status(500);
+    }
+})
+
+//Dummy Delete
+router.delete("/:postId",async(req,res)=>{
+    try{
+        const post=await Post.findByIdAndRemove({
+            _id:req.params.postId
+        });
+        res.send(post);
+    }catch(error){
+        res.status(500)
+    }
+})
+
+
 
 module.exports=router;
--- server.js
+++ server.js
@@ -13,8 +13,10 @@ require("./model/Post")
 //Middleware
 app.use(bodyParser.json())
     .use(morgan())
-    .use("/posts",require("./routes/posts.js"))
-const Post=mongoose.model("Post");
+
+
+//Routes
+app.use("/posts",require("./routes/posts"))
 
 //Test PostMan
 app.get("/post",(request,response)=>{
@@ -22,69 +24,7 @@ app.get("/post",(request,response)=>{
         name:"Apoorv"
     })
 })
-/*
-//Dummy get request
-app.get("/posts",async(request,response)=>{
-    try{
-        const posts=await Post.find({});
-        response.send(posts);
-    }catch(error){
-        response.status(500);
-    }
-});
 
-//Dummy get request wih filter
-app.get("/posts/:postId",async (req,res)=>{
-    try{
-        const post=await Post.findOne({_id:req.params.postId})//find for all the entries and findOne for first occurence
-        res.send(post);
-    }catch(error){
-        res.status(500);
-    }
-})
-
-//Dummy put request
-app.put("/posts/:postId",async(req,res)=>{
-    try{
-        const post=await Post.findByIdAndUpdate({
-            _id:req.params.postId
-        },req.body,{
-            new:true, // If new:true in addidtional information then
-            runValidators:true
-        })       
-        res.send(post)//the post object sent here is updated otherwise it is the original one but updates are made regardless  
-    }catch(error){
-        res.status(500);
-    }
-})
-
-//Dummy Delete
-app.delete("/posts/:postId",async(req,res)=>{
-    try{
-        const post=await Post.findByIdAndRemove({
-            _id:req.params.postId
-        });
-        res.send(post);
-    }catch(error){
-        res.status(500)
-    }
-})
-//Dummy post request
-app.post("/posts",async (req,res)=>{
-    try{
-
-        const post=new Post();
-        post.title=req.body.title;
-        post.content=req.body.content;
-        
-        await post.save();// Dummy Save to DB
-        res.send(post);
-    }catch(error){
-        res.status(500);
-    }
-        
-})
-*/
 //Dummy Console Check
 app.listen(7777,function(){
     console.log("Server Running at port 7777");
